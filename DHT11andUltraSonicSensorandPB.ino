3e81cf26e02c7933b146a6c83e3adbc5a27d5d32
QuocDuyPham <126590789+QuocDuyPham@users.noreply.github.com>
2023-05-05 10:49:47 -0700

    Update DHT11andUltraSonicSensorandPB.ino

--- DHT11andUltraSonicSensorandPB.ino
+++ DHT11andUltraSonicSensorandPB.ino
@@ -1,24 +1,29 @@
+// DHT Temperature & Humidity Sensor
+// Unified Sensor Library Example
+// Written by Tony DiCola for Adafruit Industries
+// Released under an MIT license.
+
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
 #include <LiquidCrystal.h>
-
+e
 LiquidCrystal lcd(2,3,9,10,11,12);
-float x; // to convert the temparture from degree to farenheit
-float temperature=0; // to store the value from event temperature
-float duration, distance; 
-float soundsp= 0; // to find the speed of the sound
-float soundcm= 0; // to convert the speed to cm/ms
+const int pB = 4;
+int buttonState;
+int value = 0; // to store the time PB has been pressed
+float x; // to convert the degree temperature to Farenhiet
+int lastButtonState = HIGH;
+float temperature=0; // to store the event temperature 
 #define trigPin 6   // This sends the trigger signal
 #define echoPin 7 // This receives the echo signal
-
-
-
-
+// Declare two float variables to record the duration and distance data.
+float duration, distance;
 #define DHTPIN 8     // Digital pin connected to the DHT sensor 
 
 // Uncomment the type of sensor in use:
 #define DHTTYPE    DHT11     // DHT 11
+// To create the degree sympol on lcd
 byte customChar[8] = {
   B11100,
   B10100,
@@ -66,40 +71,53 @@ void setup() {
   //initialize lcd display
   lcd.begin(16, 2);
   lcd.createChar(0,customChar);
-  pinMode(trigPin, OUTPUT);    // Assign trigPin as OUTPUT
-pinMode(echoPin, INPUT);     // Assign echoPin as Input
-   delayMS = sensor.min_delay / 1000;
+  pinMode(pB, INPUT_PULLUP);
+  
+
 }
 
 void loop() {
-  // Delay between measurements
-   delay(delayMS);
   // Get temperature event and print its value.
   sensors_event_t event;
   dht.temperature().getEvent(&event);
   if (isnan(event.temperature)) {
     Serial.println(F("Error reading temperature!"));
+ 
+
   }
   else {
     Serial.print(F("Temperature: "));
     Serial.print(event.temperature);
-    Serial.println(F("°C")
-// store the value of event.temperature 
+    Serial.println(F("°C"));
 temperature = event.temperature; 
   }
   // Get humidity event and print its value.
   dht.humidity().getEvent(&event);
   if (isnan(event.relative_humidity)) {
     Serial.println(F("Error reading humidity!"));
+
   }
   else {
-    Serial.print(F(" \t Humidity: "));
+    Serial.print(F("Humidity: "));
     Serial.print(event.relative_humidity);
     Serial.println(F("%"));
   
-digitalWrite(trigPin, LOW); // Sets the trigger pin (connected to the ultrasonic sensor) to LOW
+  }
+   buttonState = digitalRead(pB); // Read the value from the PB
+  if (buttonState == LOW && lastButtonState == HIGH) { // Check if button was just pressed
+  delay(50);
+  buttonState = digitalRead(pB);
+ if (buttonState == LOW) {
+   value++; 
+ if (value > 3) {
+   value = 1;
+ x = (event.temperature*9/5)+32.0;
+ }
+ }
+  }
+digitalWrite(trigPin, LOW);//The trigger pin (which is connected to the ultrasonic sensor) is set to LOW.
 delayMicroseconds(2);       // To ensure trigger pin has settle down, put delay
-digitalWrite(trigPin, HIGH); // Set the trigger pin to HIGH, which sends a 10-microsecond pulse to the ultrasonic sensor.
+digitalWrite(trigPin, HIGH); //Set the trigger pin to HIGH, and the ultrasonic sensor will receive a 10-microsecond pulse.
 delayMicroseconds(20);     // Recommended minimum time between the trigger signal and the start of the echo pulse
 digitalWrite(trigPin, LOW); // Set the trigger pin back to LOW to complete the trigger signal.
 
@@ -109,63 +127,54 @@ duration = pulseIn(echoPin, HIGH);
 distance = (duration /2 ) * 0.0343;
 
 // Print the distance on the serial monitor
-Serial.print(" \t Distance: ");
-/*If the distance is out of range (which is ess than 2cm or more than 400cm), 
-it will prints "Out of Range" instead of the distance*/
+Serial.print("Distance: ");
+/*If the distance is out of range (less than 2cm or more than 400cm), 
+it prints "Out of Range" instead of the distance
 if (distance >= 400 || distance <= 2) {
   Serial.println("Out od Range");
-// if the distance is between 2 and 400 it will print the distacne
 } else {
-  Serial.print( \t distance);
-  Serial.println(" cm")
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(500);
 }
-      // Calculate the Speed of Sound in M/S
-    soundsp = 331.4 + (0.606 * temperature) + (0.0124 * event.relative_humidity);
-    
-    // Convert to cm/ms
-    
-    soundcm = soundsp / 10000;
-}
-
-// to convert the degree temperature to Farenheit 
-//serial print that value
- x = (event.temperature*9/5)+32.0;
- serial.println("Far = ");
- serial.print(x);
- delays(500);
-// Serial print the speed of the sound 
-serial.println(" \t sound  = ");
- serial.print(x);
- delays(500);
-// lcd print out the value of humidity and degree temperature
+  lastButtonState = buttonState;
+  // use switch case to the lcd screen base on the value of PB 
+    switch (value) {
+      // case 1 display humidity and temperature
+     case 1:
      lcd.setCursor(0,0);
      lcd.print(F("Hum:"));
      lcd.print(event.relative_humidity);
      lcd.print(F("%"));
      lcd.print("  ");
-     lcd.setCursor(0,0);
-     lcd.print(F("T:"));
+     lcd.setCursor(0,1);
+     lcd.print(F("Temp:"));
      lcd.print(temperature);
      lcd.write((byte)0);
      lcd.print(F("C"));
      lcd.print("  ");
-
-lcd.setCursor(0,1);
-lcd.print("Distance: ");
-if (distance >= 400 || distance <= 2) {
-  lcd.setCursor(0,1);
-  lcd.print("Out of Range");
-} else {
-  lcd.clear();
-  lcd.setCursor(0,1);
-  lcd.print("Distance: ");
-  lcd.print(distance);
-  lcd.print("cm");
-     lcd.setCursor(0,1);
+     break ; 
+     // case 2 display Farenheit 
+     case 2:
+     lcd.clear();
+     lcd.setCursor(0,0);
      lcd.print(F("Far:"));
      lcd.print(x);
      lcd.write((byte)0);
      lcd.print(F("F"));
      lcd.print(" ");
+     break ;
+  // case 3 display the distance 
+     case 3:
+     lcd.clear();
+lcd.setCursor(0,0);
+lcd.print("Distance: ");
+if (distance >= 400 || distance <= 2) {
+  lcd.setCursor(0,1);
+  lcd.print("Out of Range");
+} else {
+  lcd.setCursor(0,1);
+  lcd.print(distance);
+  lcd.print(" cm");
 }
   }
