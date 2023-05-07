f6b1ce20170af09e5c4a90e402649857196ec88a
QuocDuyPham <126590789+QuocDuyPham@users.noreply.github.com>
2023-05-07 14:53:00 -0400

    Update ALPHA_LCD_DHT11_HFC.ino

--- ALPHA_LCD_DHT11_HFC.ino
+++ ALPHA_LCD_DHT11_HFC.ino
@@ -1,53 +1,45 @@
+
+
+// Include Adadruit_Sensor library for sensors
+#include <Adafruit_Sensor.h>
+// Include DHT library for DHT11 sensor
+#include <DHT.h>
+#include <DHT_U.h>
+// Include LiquidCrystal library
 #include <LiquidCrystal.h>
 
 
-LiquidCrystal lcd(2, 3, 9, 10, 11, 12);
+#define DHTPIN 8   // Define pin for DHT11 
+#define DHTTYPE DHT11   // Define the type sensors using for this project
+DHT_Unified dht(DHTPIN, DHTTYPE);   // To create an object called DHT
+uint32_t delayMS;   // To define an unsigned integer for a delay in milliseconds
 
-byte customChar[8] = { 
-    
-  0b00000,
-	0b11000,
-	0b11000,
-	0b00000,
-	0b00000,
-	0b00000,
-	0b00000,
-	0b00000
+// Initialize the pins we using for the lcd
+const int rs = 2, en = 3, d4 = 9, d5 = 10, d6 = 11, d7 = 12;
+LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
+
+// Define the degree character for the LCD
+byte customChar[8] = {
+  B11100,
+  B10100,
+  B11100,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000
 };
 
-
-
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include <DHT_U.h>
-
-#define DHTPIN 7     // Digital pin connected to the DHT sensor 
-
-// Uncomment the type of sensor in use:
-//#define DHTTYPE    DHT11     // DHT 11
-#define DHTTYPE    DHT11    // DHT 22 (AM2302)
-//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
-
-DHT_Unified dht(DHTPIN, DHTTYPE);
-
-uint32_t delayMS;
-
-const int buttonPin = 8;
-int buttonState = LOW;
-int lastButtonState = LOW:
-
-unsigned long lastDebounceTime = 0;
-unsigned long debounceDelay = 50;
-
-int count = 0;
-
-
 void setup() {
-  Serial.begin(9600);
-  lcd.createChar(0, customChar); 
-  lcd.begin(16, 2); //lcd display will take 16 coloums and 2 rows 
-  // Initialize device.
-  dht.begin();
+  // Initialize Serial Monitor 
+  Serial.begin(9600); 
+  // Initialize DHT11
+  dht.begin();    
+  // Set up the LCD's number of columns and rows    
+  lcd.begin(16, 2);     
+  // Create a custom character to display degree symbol 
+  lcd.createChar(0,customChar);  
+
   Serial.println(F("DHTxx Unified Sensor Example"));
   // Print temperature sensor details.
   sensor_t sensor;
@@ -62,7 +54,6 @@ void setup() {
   Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
   Serial.println(F("------------------------------------"));
   // Print humidity sensor details.
-
   dht.humidity().getSensor(&sensor);
   Serial.println(F("Humidity Sensor"));
   Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
@@ -74,63 +65,61 @@ void setup() {
   Serial.println(F("------------------------------------"));
   // Set delay between sensor readings based on sensor details.
   delayMS = sensor.min_delay / 1000;
- 
 }
 
 void loop() {
-
-  // Delay between measurements.
-  
-  delay(delayMS);
+  // to print temperature and humdity on the Serial Monitor
+  delay(delayMS);     // Delay between measurements.
   // Get temperature event and print its value.
   sensors_event_t event;
   dht.temperature().getEvent(&event);
+  // If temperature is not a number then it will print Error reading temperature
   if (isnan(event.temperature)) {
     Serial.println(F("Error reading temperature!"));
-
-    delay(1000);
   }
+  // Print temperature value 
   else {
-    Serial.print(F("Temperature Celcius: "));
+    Serial.print(F("Temperature: "));
     Serial.print(event.temperature);
     Serial.println(F("°C"));
-
-    Serial.print(F("Temperature Fahrenheit: "));
-    float Fvalue = ((event.temperature*9/5) + 32);
-    Serial.print(Fvalue);
-    Serial.println(F("°F"));
-
-    lcd.setCursor(0,0);
-    lcd.print("T=" );
-    lcd.print(event.temperature);
-    lcd.write((byte)0);
-    lcd.print("C");
-    lcd.print(Fvalue);
-    lcd.write((byte)0);
-    lcd.print("F");
-
-  
   }
   // Get humidity event and print its value.
-  dht.humidity().getEvent(&event);
+    dht.humidity().getEvent(&event);
+  // If humidity is not a number then it will print Error reading temperature
   if (isnan(event.relative_humidity)) {
     Serial.println(F("Error reading humidity!"));
-
-    delay(1000);
   }
+  // Print Humidity value
   else {
     Serial.print(F("Humidity: "));
     Serial.print(event.relative_humidity);
     Serial.println(F("%"));
-  
-
-    lcd.setCursor(0,1);
-    lcd.print("H= ");
-    lcd.print(event.relative_humidity);
-    lcd.print("%");
-    lcd.print("         ");
-
-  
   }
-   
+  
+  // Display temperature and humidity data from DHT11 sensor on the LCD Screen
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    lcd.setCursor(0,0);
+    lcd.print(F("Error reading temperature!"));
+  }
+  // Display temperature with degree symbol on LCD
+  else {
+    lcd.setCursor(0,0);    
+    lcd.print(F("Temp:")); 
+    lcd.print(event.temperature);  
+    lcd.write((byte)0);    
+    lcd.print(F("C"));     
+  }
+    dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)){
+    lcd.setCursor(0,1);
+    lcd.print(F("Error reading humidity!"));
+  }
+  // Display humidty on LCD 
+  else {
+    lcd.setCursor(0,1);
+    lcd.print(F("Hum:"));
+    lcd.print(event.relative_humidity);
+    lcd.print(F("%"));
+  }
 }
